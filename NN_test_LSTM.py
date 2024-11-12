2bb59d03ccca203965f3f0e51ef1f5ca31db23d9
Slowpok <yuligalakhova@yandex.ru>
2024-11-12 11:06:19 +0300

    Update NN_test_LSTM.py

--- NN_test_LSTM.py
+++ NN_test_LSTM.py
@@ -7,7 +7,7 @@ import torch.utils.data as data
 from keras._tf_keras.keras.preprocessing.sequence import pad_sequences
 import LSTM
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # dannye
 ee = pd.read_csv(filepath_or_buffer=NN_init.finename,
@@ -18,18 +18,16 @@ y_mass = ee['class'] # dlya binarnoy
 
 x_mass = pad_sequences(Datasets.string_list_to_sequence(x_mass), NN_init.size_of_array)
 
-x_mass = torch.Tensor(x_mass).to(device)
-y_mass = torch.as_tensor(y_mass).to(device)
+x_mass = torch.Tensor(x_mass).to(NN_init.device)
+y_mass = torch.as_tensor(y_mass).to(NN_init.device)
 
 MyDataset = Datasets.MyDataset(x_mass, y_mass)
 
 train_loader = data.DataLoader(MyDataset, batch_size=NN_init.batch_size, shuffle=True, pin_memory=False)
 val_loader = data.DataLoader(MyDataset, batch_size=NN_init.batch_size, shuffle=True, pin_memory=False)
 
-
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model_LSTM = LSTM.LSTM_Model(name="LSTM", size_token=NN_init.size_of_array, unique_words=x_mass.shape[0],num_classes=1)
-model_LSTM.to(device)
+model_LSTM.to(NN_init.device)
 
 loss_fn = torch.nn.BCELoss()
 optimizer = torch.optim.Adam(model_LSTM.parameters(), lr=1e-2, amsgrad=True)
