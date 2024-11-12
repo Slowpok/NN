6b00acdd938ce9657bfe217eb8b03a9cd249e87b
Slowpok <yuligalakhova@yandex.ru>
2024-11-12 10:59:35 +0300

    Update NN_init.py

--- NN_init.py
+++ NN_init.py
@@ -4,3 +4,4 @@ learning_rate = 0.001
 batch_size = 10000
 finename = r"C:\Users\79118\OneDrive\Документы\Нейросет\Материалы датасет3.csv"
 num_classes = 2
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
